95021587703b8556250d47d346aa95308e0736f0
Ivelina Doynova <ivelina.doynova@gmail.com>
2019-06-27 16:50:43 +0300

    Add buttons

--- shadopsapp/views.py
+++ shadopsapp/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'shadopsapp/index.html')
+    return render(request, 'shadopsapp/base.html')
 
 
 def about(request):
