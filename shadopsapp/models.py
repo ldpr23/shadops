092011c0f78d6cd0f08525c54d59d58269d36ca5
kimbethstonehouse <kimbethstonehouse@gmail.com>
2019-06-27 17:07:21 +0100

    changed to django

--- shadopsapp/models.py
+++ shadopsapp/models.py
@@ -1,47 +1,6 @@
 from django.db import models
 
 # Create your models here.
-<<<<<<< HEAD
-class Person(models.Model):
-  def __init__(self, id, fname, lname, loc, lang, tech, lob):
-
-    # current info about job#
-    self.id = id
-    self.fname = fname
-    self.lname = lname
-    self.loc = loc
-    self.lang = lang
-    self.tech = Technology(tech)
-    self.lob = lob
-      
-    # desired info for shadowing
-    self.desiredLoc = []
-    self.desiredLang = []
-    self.desiredTech = []
-    self.desiredLob = []
-
-    # setters
-    def set_loc(self, desiredLoc): 
-        self.desiredLoc = desiredLoc
-        
-    def set_lang(self, desiredLang): 
-        self.desiredLang = desiredLang
-
-    def set_tech(self, desiredTech): 
-        self.desiredTech = desiredTech 
-
-    def set_lob(self, desiredLob): 
-        self.desiredLob = desiredLob
-
-class Language():
-    def __init__(self, lang):
-        self.lang = lang
-
-class Tecnology():
-    def __init__(self, tech):
-        self.tech = tech
- 
-=======
 class LineOfBusiness(models.Model):
     name = models.CharField(max_length = 50, unique=True)
 
@@ -53,16 +12,45 @@ class Location(models.Model):
     city = models.CharField(max_length = 50, blank=False)
     building = models.CharField(max_length = 50, blank=False)
 
+# class Language(models.Model):
+#     language = models.CharField(max_length=20, unique=True)
+#     slug = models.SlugField(max_length=40, unique=True)
+
+#     class Meta:
+#         verbose_name_plural = 'Languages'
+
+#     def __str__(self):
+#         return self.name
+
+#     def save(self, *args, **kwargs):
+#         self.slug = slugify(self.name)
+#         super(Language, self).save(*args, **kwargs)
+
+# class Technology(models.Model):
+#     technolgy = model.CharField(max_length=30, unique=True)
+#     slug = models.SlugField(max_length=40, unique=True)
+
+#     class Meta:
+#         verbose_name_plural = 'Technologies'
+
+#     def __str__(self):
+#         return self.name
+
+#     def save(self, *args, **kwargs):
+#         self.slug = slugify(self.name)
+#         super(Technology, self).save(*args, **kwargs)
+
 class DesiredInfo(models.Model):
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
-    lob = models.ForeignKey(LineOfBusiness, on_delete=models.CASCADE)
-    language = models.CharField(max_length = 50, blank=True)
+    lob = models.ManyToManyField(LineOfBusiness, on_delete=models.CASCADE)
+    technology = models.ManyToManyField(Technology, on_delete=models.CASCADE)
+    language = models.ManyToManyField(Language, on_delete=models.CASCADE)
 
 class Employee(models.Model):
     firstName = models.CharField(max_length = 20, blank=False)
     surname = models.CharField(max_length = 20, blank=False)
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
     lob = models.ForeignKey(LineOfBusiness, on_delete=models.CASCADE)
-    language = models.CharField(max_length = 50, blank=False)
+    language = models.ManyToManyField(Language, on_delete=models.CASCADE)
+    technology = models.ManyToManyField(Technology, on_delete=models.CASCADE)
     desiredInfo = models.ForeignKey(DesiredInfo, on_delete=models.CASCADE)
->>>>>>> 5be838323ae355870d5f7532eb97bf29eb34b67a
