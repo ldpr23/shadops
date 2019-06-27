099737b0372a0aaf52cd964200f9c693ee1cee58
kimbethstonehouse <kimbethstonehouse@gmail.com>
2019-06-27 13:35:40 +0100

    created person object

---
+++ shadops/main.py
@@ -0,0 +1,9 @@
+from person import person
+
+def main():
+    kim = person("Kim", "Stonehouse", "React", "Frontend", "CIB", "Glasgow")
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ shadops/person.py
@@ -0,0 +1,29 @@
+class person:
+  def __init__(self, fname, lname, lang, tech, lob, loc):
+
+    # basic info about current job
+    self.fname = fname
+    self.lname = lname
+    self.lang = lang
+    self.tech = tech
+    self.lob = lob
+    self.loc = loc
+      
+    # desired info for shadowing
+    self.desiredLoc = ""
+    self.desiredLang = ""
+    self.desiredTech = ""
+    self.desiredLob = ""
+
+    # setters
+    def set_loc(self, desiredLoc): 
+        self.desiredLoc = desiredLoc
+        
+    def set_lang(self, desiredLang): 
+        self.desiredLang = desiredLang
+
+    def set_tech(self, desiredTech): 
+        self.desiredTech = desiredTech 
+
+    def set_lob(self, desiredLob): 
+        self.desiredLob = desiredLob 
