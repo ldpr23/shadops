a551fa5c39429d247043d31f854440edf97d08e2
Ivelina Doynova <ivelina.doynova@gmail.com>
2019-06-27 16:59:36 +0300

    Add frontend

--- shadopsapp/urls.py
+++ shadopsapp/urls.py
@@ -3,5 +3,6 @@ from shadopsapp import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'about/$', views.about, name='about')
+    url(r'about/$', views.about, name='about'),
+    url(r'base/$', views.about, name='base')
 ]
